9964ae1b631b295bf537a953c32ab74b2b80a7d5
Mannyking01230 <mannyking609@gmail.com>
2024-07-19 22:51:52 +0800

    Changes

---
+++ qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"669a7d109335410018c4d306",
+				"669a7d109335410018c4d309"
+			]
+		},
+		"669a7d109335410018c4d306": {
+			"id": "669a7d109335410018c4d306",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"669a7d109335410018c4d309": {
+			"id": "669a7d109335410018c4d309",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
